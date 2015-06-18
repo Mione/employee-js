99dc6fe7f127de5374b969209678c1d2993a1d90
Mio <ionescu.m@outlook.com>
2015-06-18 16:22:18 +0300

    master almost there

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-
+(function ($) {
+  $(document).ready(function () {
+    //var declaration
+    var $arrowToggle = $('.toolbox__arrow');
+    
+    //functions
+    
+    //events
+    $arrowToggle.on('click', function () {
+      var $this = $(this), isOpen, $rowContent = $('.tbl-row__content');
+        if ($this.hasClass('.toolbox__arrow--open')) {
+          $rowContent.removeClass('open');
+          $rowContent.addClass('closed');
+        }
+      })
+    
+    });
+  }(window.jQuery));
