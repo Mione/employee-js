9c21b43e4c58652378afa75f948060d4792dea22
Mio <ionescu.m@outlook.com>
2015-06-19 15:22:55 +0300

    master - adding in data from js file

---
+++ js/data/data.js
@@ -0,0 +1,57 @@
+(function (window) {
+    'use strict';
+    var data = {},
+        employees = [
+            {
+                "Full Name" : 'Ciba Mumu',
+                "Job Title" : "UI Developer",
+                "Grade" : "Senior Engineer",
+                "Allocation status" : "Allocated",
+                "Project" : "Trinity Mirror",
+                "Date of booking" : "2014-05-10"
+            },
+            {
+                "Full Name" : 'Ciba Mumu',
+                "Job Title" : "UI Developer",
+                "Grade" : "Senior Engineer",
+                "Allocation status" : "Allocated",
+                "Project" : "Trinity Mirror",
+                "Date of booking" : "2014-05-10"
+            },
+            {
+                "Full Name" : 'Ciba Mumu',
+                "Job Title" : "UI Developer",
+                "Grade" : "Senior Engineer",
+                "Allocation status" : "Allocated",
+                "Project" : "Trinity Mirror",
+                "Date of booking" : "2014-05-10"
+            },
+            {
+                "Full Name" : 'Ciba Mumu',
+                "Job Title" : "UI Developer",
+                "Grade" : "Senior Engineer",
+                "Allocation status" : "Allocated",
+                "Project" : "Trinity Mirror",
+                "Date of booking" : "2014-05-10"
+            },
+            {
+                "Full Name" : 'Ciba Mumu',
+                "Job Title" : "UI Developer",
+                "Grade" : "Senior Engineer",
+                "Allocation status" : "Allocated",
+                "Project" : "Trinity Mirror",
+                "Date of booking" : "2014-05-10"
+            },
+            {
+                "Full Name" : 'Ciba Mumu',
+                "Job Title" : "UI Developer",
+                "Grade" : "Senior Engineer",
+                "Allocation status" : "Allocated",
+                "Project" : "Trinity Mirror",
+                "Date of booking" : "2014-05-10"
+            }
+        ];
+    data.employees = employees;
+    window.data = data;
+
+}(window))
