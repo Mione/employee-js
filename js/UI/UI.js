6ec6bb37619852b245a66a8aa008a000c297c09f
Mio <ionescu.m@outlook.com>
2015-06-19 13:04:05 +0300

    master - adding a bit of js for editable toggle

--- js/UI/UI.js
+++ js/UI/UI.js
@@ -2,8 +2,9 @@
     'use strict';
     $(document).ready(function () {
     //var declaration
-        var $arrowToggle = $('.toolbox__arrow');
-
+        var $arrowToggle = $('.toolbox__arrow'),
+            $editToggle = $('.toolbox__pencil'),
+            inEditMode = false;
         //functions
         //events
         $arrowToggle.on('click', function () {
@@ -21,6 +22,36 @@
                 $details.hide('fast');
                 $details.removeClass('open');
                 $details.addClass('closed');
+            }
+        });
+        $editToggle.on('click', function () {
+            var $this = $(this),
+                $details = $this.closest('.tbl-row__header').siblings('.tbl-row__content').find('.row-content__details');
+            if (inEditMode === false) {
+                var $listItems = $.makeArray($details.children()),
+                    $base = $('<ul>');
+                $base.addClass('row-content__details');
+                $.each($listItems, function (index, item) {
+                    var text = $(item).text(),
+                        $li = $('<li>'),
+                        $input = $('<input>').attr({
+                            type : 'text',
+                            value : text,
+                            name : 'editableInput'
+                        });
+                    $input.appendTo($li);
+                    $li.appendTo($base);
+                });
+                $details.empty();
+                $details.replaceWith($base);
+                inEditMode = true;
+            } else {
+                var editableInputs = $("[name = 'editableInput']");
+                $.each(editableInputs, function (index, item) {
+                    var value = $(item).val();
+                    $(item).replaceWith(value);
+                });
+                inEditMode = false;
             }
         });
     });
