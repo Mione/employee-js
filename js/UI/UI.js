5bd8d3258ad973dd20c137bb3bce00cb8455b1e4
Mio <ionescu.m@outlook.com>
2015-06-23 15:29:13 +0300

    master - adding header generated data :D

--- js/UI/UI.js
+++ js/UI/UI.js
@@ -1,21 +1,54 @@
 (function ($) {
     'use strict';
     $(document).ready(function () {
+      
         //var declaration
         var $arrowToggle = $('.toolbox__arrow'),
             $editToggle = $('.toolbox__pencil'),
             inEditMode = false,
-            employees = window.data;
+            $toolboxControlls = $(".employee-tbl"),
+            employees = window.data.employees;
+            generateData(employees);
         //functions
         //TODO continue with building the data.
+        function createLi(innerText, attr, attrVal, parent) {
+            var createdLi = $('<li>');
+            createdLi.attr(attrVal);
+            createdLi.text(innerText);
+            createdLi.appendTo(parent);
+        }
+        
         function generateData(list) {
-            $.each(list, function (index, item) {
-                var base = $('<div>').addClass('employee-tbl__row clearfix'),
+            var parentContainer = $(".employee-tbl");
+            $.each(list, function (key, value) {
+                var base = $('<div>').addClass('employee-tbl__row'),
                     baseheader = $('<ul>').addClass('tbl-row__header'),
                     baseContent = $('<ul>').addClass('tbl-row__content closed'),
-                    $li = $('<li>').attr('data-editable', '').text();
+                    toolbox = "<li class=\"toolbox\">"+
+                    "<div data-editable = \"date\" class=\"toolbox__date\">" + value["Date of booking"] + "</div>"+
+                    "<div class=\"toolbox__controlls\">"+
+                      "<span class=\"toolbox__pencil\"></span>"+
+                      "<span class=\"toolbox__arrow toolbox__arrow--closed\"></span>"+
+                    "</div>"+
+                  "</li>";
+                
+                createLi(value["Full Name"], "data-editable", "", baseheader);
+                createLi(value["Job Title"], "data-editable", "", baseheader);
+                createLi(value["Grade"], "data-editable", "", baseheader);
+                createLi(value["Allocation status"], "data-editable", "", baseheader);
+                createLi(value["Project"], "data-editable", "", baseheader);
+                $(toolbox).appendTo(baseheader);
+                
+                createLi(value["Full Name"], "data-editable", "", baseContent);
+                createLi(value["Job Title"], "data-editable", "", baseContent);
+                createLi(value["Full Name"], "data-editable", "", baseContent);
+                createLi(value["Job Title"], "data-editable", "", baseContent);
+                baseheader.appendTo(base);
+                baseContent.appendTo(base);
+                base.appendTo(parentContainer);
             });
         }
+        
         function isDate($obj) {
             if ($obj.data('editable') === 'date') {
                 return 'date';
@@ -24,7 +57,7 @@
             }
         }
         //events
-        $arrowToggle.on('click', function () {
+        $toolboxControlls.on('click', '.toolbox__arrow', function () {
             var $this = $(this),
                 $details = $this.closest('.tbl-row__header').siblings('.tbl-row__content');
             if ($this.hasClass('toolbox__arrow--closed')) {
@@ -41,7 +74,7 @@
                 $details.addClass('closed');
             }
         });
-        $editToggle.on('click', function () {
+        $toolboxControlls.on('click', '.toolbox__pencil', function () {
             var $this = $(this),
                 $details = $this.closest('.employee-tbl__row').find('[data-editable]');
             if (inEditMode === false) {
